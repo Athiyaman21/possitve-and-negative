a223bd4dca74d6848cd94d8ea49e5c7a825594e9
Athiyaman21 <42763397+Athiyaman21@users.noreply.github.com>
2018-09-11 16:10:20 +0600

    Add files via upload

---
+++ JavaApplication7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.*;
+import java.lang.*;
+
+class JavaApplicaton7
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int num;
+		 Scanner  sc= new Scanner(System.in);
+		
+			 System.out.println("entre the num");
+		num= sc .nextInt();
+		
+   	                if(num%2==0)
+		{
+			System.out.println("it is a even");
+		}
+                       
+                           else
+                                   {
+             		System.out.println("it is a odd");
+		            }
+                       }
+             }
+             
