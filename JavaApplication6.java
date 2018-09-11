258267a38aeb7b1eb71f32c5c5737db0e86b0b98
Athiyaman21 <42763397+Athiyaman21@users.noreply.github.com>
2018-09-11 15:31:06 +0600

    Add files via upload

---
+++ JavaApplication6.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.io.*;
+import java.lang.*;
+
+        public class JavaApplication6
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num;
+		 Scanner  sc= new Scanner(System.in);
+		
+			 System.out.println("entre the num");
+		num= sc .nextInt();
+		
+   	                if(num==0)
+		{
+			System.out.println("it is a zero");
+		}
+                       else if (num>0)
+                                   {
+             		System.out.println("it is a posstive");
+		             }
+                           else
+                                   {
+             		System.out.println("it is a negative");
+		            }
+                       }
+             }
+             
