13c8e0fa6f4134d6b0302ec4488860262fe632a7
Athiyaman21 <42763397+Athiyaman21@users.noreply.github.com>
2018-09-18 14:49:20 +0600

    Add files via upload

---
+++ JavaApplication10.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class JavaApplication10
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        //char ch;
+        Scanner s= new Scanner(System.in);
+                System.out.println("entre the character");
+                char c =s.next().charAt(0); 
+                {
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+               
+               
+        else
+            System.out.println(c + " is not an alphabet.");
+                }
+                return(0);
+    }
+                
+}     
