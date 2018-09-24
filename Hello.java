31dacb907cf6f958e7bf90bfa399c7628c47b2c2
Athiyaman21 <42763397+Athiyaman21@users.noreply.github.com>
2018-09-24 10:45:10 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+import java .util.*;
+import java.io.*;
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        int i=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the N value");
+         int N=sc.nextInt();
+        while(i<N)
+        {
+            System.out.println("Hello");
+            i++;
+    }
+}
+}
