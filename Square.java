32636960655282549489fbe0fe455514c782aa4c
Athiyaman21 <42763397+Athiyaman21@users.noreply.github.com>
2018-09-24 14:35:17 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java .io.*;
+public class Square
+{
+      public static void main (String[] args)
+      {
+          int k,l=1;
+                  
+          Scanner sc=new Scanner(System.in);
+          System.out.println("entre the k");
+          k=sc.nextInt();
+          System.out.println("entre the l");
+          l=sc.nextInt();
+          {
+              math.pow(k,l);
+        //System.out.println("Math.pow(" + k + "," + l+ ")=" + Math.pow(k, l));
+            
+          }
+      
+      }
+}
